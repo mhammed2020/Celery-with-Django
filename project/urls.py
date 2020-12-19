4f1945d011dfce1266d1bca9a1157f9e8cc1e45f
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-19 15:52:30 +0100

    update  main urls.py

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('example/', include('example.urls'))
+    path('', include('example.urls'))
 
 ]
